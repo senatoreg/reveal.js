85c73b255bb61388bde40c8e3388e1a212cdb678
Giovanni Senatore <giovanni.senatore@gmail.com>
2022-01-13 23:55:06 +0100

    Add style option for background

--- js/controllers/backgrounds.js
+++ js/controllers/backgrounds.js
@@ -127,6 +127,7 @@ export default class Backgrounds {
 			backgroundTransition: slide.getAttribute( 'data-background-transition' ),
 			backgroundOpacity: slide.getAttribute( 'data-background-opacity' ),
 			backgroundClass: slide.getAttribute( 'data-background-class' ) ? slide.getAttribute( 'data-background-class' ).split(' ') : null,
+			backgroundStyle: slide.getAttribute( 'data-background-style' ) ? slide.getAttribute( 'data-background-style' ).split(';') : null,
 		};
 
 		const dataPreload = slide.hasAttribute( 'data-preload' );
@@ -188,6 +189,7 @@ export default class Backgrounds {
 		if( data.backgroundPosition ) contentElement.style.backgroundPosition = data.backgroundPosition;
 		if( data.backgroundOpacity ) contentElement.style.opacity = data.backgroundOpacity;
 		if( data.backgroundClass ) data.backgroundClass.forEach((c, i) => { contentElement.classList.add(c); });
+		if( data.backgroundStyle ) data.backgroundStyle.forEach((s, i) => { let p = s.split(':'); let v = p[1].split('!'); contentElement.style.setProperty(p[0], v[0], v[1]); });
 
 		// If this slide has a background color, we add a class that
 		// signals if it is light or dark. If the slide has no background
